0d34cdad2fa15d77e3ff3ed51188fb096fa59f5c
pmduc010702 <pmduc010702@gmail.com>
2025-06-29 22:38:59 +0700

    Display Booked appointments on Website

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -185,7 +185,20 @@ const bookAppointment = async (req, res) => {
         return res.json({ success: false, message: error.message });
     }
 };
-  
+
+//API to get user appointments
+
+const listAppointment = async (req, res) => {
+    try {
+        const userId = req.user.id; 
+        const appointments = await appointmentModel.find({ userId })
+
+        res.json({ success: true, appointments })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment }
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment }
